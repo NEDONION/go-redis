cd091f3616ab048d99460f517589c2bf5ee1a538
NEDONION <nedjiachenghu@gmail.com>
2023-03-27 18:23:01 -0500

    feat: dict interface init

---
+++ datastruct/dict/dict.go
@@ -0,0 +1,20 @@
+package dict
+
+// Consumer is used to traversal dict, if it returns false the traversal will be break
+// 用于遍历字典的函数，如果返回 false 则遍历会被中断
+type Consumer func(key string, val interface{}) bool
+
+// Dict is interface of a key-value data structure
+type Dict interface {
+	Get(key string) (val interface{}, exists bool)
+	Len() int
+	Put(key string, val interface{}) (result int)
+	PutIfAbsent(key string, val interface{}) (result int)
+	PutIfExists(key string, val interface{}) (result int)
+	Remove(key string) (result int)
+	ForEach(consumer Consumer)
+	Keys() []string
+	RandomKeys(limit int) []string
+	RandomDistinctKeys(limit int) []string
+	Clear()
+}
